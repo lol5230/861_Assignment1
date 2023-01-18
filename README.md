ebcd0f6bed9d26f1205ef4cab7b038afdbf3ba73
Luke Leiter <lol5230@psu.edu>
2023-01-18 12:25:25 -0500

    Inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 861_Assignment1
+This is the source code for assignment 1 for SWENG 861. 
---
+++ src/main/java/psu/lol5230/_assignment1/App.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package psu.lol5230._assignment1;
+
+/**
+ *
+ * @author luke.leiter
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println( helloWorld());
+    }
+    
+    public static String helloWorld()
+    {
+        return "Hello World! This is from a Method";
+    }
+}
